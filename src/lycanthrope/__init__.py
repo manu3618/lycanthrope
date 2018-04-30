9ed73bdf32a7deaa3835a0f2c113e1724cfd0b59
manu <you@example.com>
2018-04-30 19:41:46 +0200

    Add deal_role method.

--- src/lycanthrope/__init__.py
+++ src/lycanthrope/__init__.py
@@ -1,3 +1,20 @@
+from random import shuffle
+
+
+# max number of playerr with the role
+MAX_ROLE_NB = {'tanneur': 1,
+               'chasseur': 1,
+               'doppelgänger': 1,
+               'loup garou': 2,
+               'sbire': 1,
+               'franc maçon': 2,
+               'voyante': 1,
+               'voleur': 1,
+               'noiseuse': 1,
+               'soulard': 1,
+               'insomniaque': 1}
+
+
 class Game:
     """Game
 
@@ -6,8 +23,51 @@ class Game:
 
     Attributes:
         role (list): list of roles played for this game
-        players (list): list of  player's nick.
+        players (list): list of  player's nick. The firs  players, identified
+    by numbers, represent the role in the middle (not real players)
+        initial_roles (dict): initial distribution of roles. Looks like:
+    {player_nick: role}
+        current_roles (dict): current distribution of roles.
     """
-    roles = []
-    players = []
 
+    def __init__(self):
+        self.roles = []
+        self.initial_roles = {}
+        self.current_roles = {}
+
+        # initialize players with roles in the middle
+        self.players = [str(num) for num in range(3)]
+
+    def add_player(self, nick):
+        """Add the player <nick> to the game.
+
+        Only works befor the game is started
+
+        Args:
+            nick (string): nick of the player
+        """
+        if len(self.players) >= sum(MAX_ROLE_NB.values()):
+            raise RuntimeWarning("Maximum number of players reached")
+        if nick not in self.players:
+            self.players.append(nick)
+
+    def deal_roles(self):
+        """Maps each player to a role."""
+        nb_player = len(self.players)
+        nb_role = nb_player + 3
+
+        # select roles
+        roles = [role for role, nb in MAX_ROLE_NB.items()
+                 for _ in range(nb)]
+        shuffle(roles)
+
+        # at least one 'voyante' and one 'loup garou' must be dealt
+        selected_roles = roles[:nb_role]
+        if 'voyante' not in selected_roles:
+            selected_roles[0] = 'voyante'
+        if 'loup garou' not in selected_roles:
+            selected_roles[1] = 'loup garou'
+        shuffle(selected_roles)
+
+        self.initial_roles = dict(zip(self.players, selected_roles))
+        self.current_roles = self.initial_roles.copy()
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,5 +1,48 @@
+from collections import Counter
+
+import faker
+import pytest
+
 import lycanthrope
+from lycanthrope import MAX_ROLE_NB
 
 
 def test_init():
     lycanthrope.Game()
+
+
+def iter_name_lists(max_len=9):
+    """Yield set of names.
+
+    Args:
+        max_len (int): maximal list length
+    """
+    names_gen = faker.Faker()
+    for length in range(3, max_len):
+        yield {names_gen.first_name() for _ in range(length)}
+
+
+@pytest.mark.parametrize('players', iter_name_lists())
+def test_deal_role(players):
+    """Deal roles among players and perform checks.
+
+    Args:
+        players (iterable): list/tuple/set of player's nick.
+    """
+    game = lycanthrope.Game()
+
+    # Add players
+    for player in players:
+        game.add_player(player)
+    assert len(game.players) == len(players) + 3
+
+    # Roles exist
+    game.deal_roles()
+    dealt_roles = game.initial_roles
+    for role in dealt_roles.values():
+        assert role in MAX_ROLE_NB
+
+    # Role number's constraints are respected
+    dealt_roles_distr = Counter(dealt_roles.values())
+    for role, nb in dealt_roles_distr.items():
+        assert nb <= MAX_ROLE_NB[role]
